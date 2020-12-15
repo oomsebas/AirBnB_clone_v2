2333f76e9bc0522d587da1ae70d6c5959118109e
Sebastian_Orozco <omsebas@gmail.com>
2020-12-15 23:11:54 +0000

    Add libraries

--- models/review.py
+++ models/review.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from os import getenv
+from sqlalchemy.orm import relationship
+import models
 
-
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = 'reviews'
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
