4e22a38900cbcf870f356cbb028e05ab79320c1a
Sebastian_Orozco <omsebas@gmail.com>
2020-12-16 00:31:50 +0000

    Task 10

--- models/place.py
+++ models/place.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
 from os import getenv
 from sqlalchemy.orm import relationship
 import models
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -20,12 +27,15 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float)
     longitude = Column(Float)
-    """ amenity_ids = []"""
+    amenity_ids = []
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         reviews = relationship("Review",
                                backref="place",
                                cascade="all, delete, delete-orphan")
+        amenities = relationship("Amenity",
+                                 secondary=place_amenity,
+                                 viewonly=False)
     else:
         @property
         def reviews(self):
@@ -37,3 +47,19 @@ class Place(BaseModel, Base):
                 if review.state_id == self.id:
                     ls.append(review)
             return ls
+
+        @property
+        def amenities(self):
+            """returns the list of City instances with state_id equals
+            to the current State.id"""
+            obj = models.storage.all(Amenity)
+            ls = []
+            for amenity in obj.values():
+                if amenity.place_id == self.id:
+                    ls.append(amenity)
+            return ls
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            if obj and obj.__class__.__name__ == "Amenity":
+                self.amenity_ids.append(obj.id)
