ec538f008be890dea876c82cbe8ffb55db5eb9fb
Ana-Morales <moralesanaruth@gmail.com>
2020-12-16 16:08:37 +0000

    Add more tests

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -12,13 +12,44 @@ from models.state import State
 from models.user import User
 from models.engine.file_storage import DBStorage
 import os
+import MySQLdb
+
+
+db = MySQLdb.connect(user=getenv('HBNB_MYSQL_USER'),
+                     passwd=getenv('HBNB_MYSQL_PWD'),
+                     db=getenv('HBNB_MYSQL_DB'))
+
+tbl_cls = {"states": State, "cities": City, "places": Place,
+           "amenities": Amenity, "users": User, "reviews": Review}
 
 
 class test_dbStorage(unittest.TestCase):
     """Tests for db storage mode"""
 
-    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != db,
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != "db",
                      "only test for db storage, not file storage")
     def test_doc(self):
         """Test docstrings"""
         self.assertIsNotNone(DBStorage.__doc__)
+
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != "db",
+                     "only test for db storage, not file storage")
+    def test_all(self):
+        """Test all method"""
+        d = models.storage.all()
+        self.assertTrue(type(d), dict)
+
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != "db",
+                     "only test for db storage, not file storage")
+    def test_new(self):
+        """Test new method"""
+        cur = db.cursor()
+        for tbl in tbl_cls:
+            rows = cur.execute("SELECT COUNT (*) FROM {}".format(tbl))
+            obj1 = tbl_cls[tbl]()
+            models.storage.new(obj1)
+            models.storage.save()
+            rows_final = cur.execute("SELECT COUNT (*) FROM {}".format(tbl))
+            self.assertTrue(rows + 1, rows_final)
+        cur.close()
+        db.close()
