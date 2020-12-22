c6036c299e9f885ea88378e70c006db2bc5fcc56
Ana-Morales <moralesanaruth@gmail.com>
2020-12-22 17:26:02 +0000

    fix command ln

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,5 +7,5 @@ mkdir -p /data/web_static/shared/
 echo "Testing file" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i '/http {/a \\tserver { \n\t\tlocation /hbnb_static/ {\n\t\t\talias /data/web_static/current/;\n\t\t}\n\t}\n' /etc/nginx/nginx.conf
+sed -i '/http {/a \\tserver { \n\t\tlocation /hbnb_static {\n\t\t\talias /data/web_static/current/;\n\t\t}\n\t}\n' /etc/nginx/nginx.conf
 service nginx restart
