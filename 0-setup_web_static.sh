6db15fc1c030ef37e0ce4a38e6a22896d7884712
Ana-Morales <moralesanaruth@gmail.com>
2020-12-22 19:01:31 +0000

    cahnge line to use sed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,5 +7,5 @@ mkdir -p /data/web_static/shared
 echo "Testing file" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i '/listen [::]:80 default_server;/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
+sed -i '/listen 80 default_server;/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
 service nginx restart
