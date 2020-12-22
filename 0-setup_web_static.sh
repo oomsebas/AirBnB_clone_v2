6680d72ca998938ba13588be9ee5fd6986cc4932
Sebastian_Orozco <omsebas@gmail.com>
2020-12-22 23:18:59 +0000

    Task 1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment
 sudo apt-get -y update
-sudo apt_get install -y nginx
+sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 echo "Holberton school" | sudo tee /data/web_static/releases/test/
