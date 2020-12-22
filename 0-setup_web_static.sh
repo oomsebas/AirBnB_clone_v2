70ee5f63472e6c43337a08ab81b24ed988e8bcd0
Ana-Morales <moralesanaruth@gmail.com>
2020-12-22 18:45:21 +0000

    back

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,8 +2,8 @@
 # Sets up your web servers for the deployment of web_static
 sudo apt-get -y update
 sudo apt-get -y install nginx
-mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 echo "Testing file" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
