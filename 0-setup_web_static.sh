54ae24f6158161b139e6d75d52f2558e5b91c112
Ana-Morales <moralesanaruth@gmail.com>
2020-12-22 17:00:14 +0000

    first commit

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Sets up your web servers for the deployment of web_static
+sudo apt-get -y update
+sudo apt-get -y install nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Testing file" > /data/web_static/releases/test/index.html
+ls -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i '/http {/a \\tserver { \n\t\tlocation /hbnb_static/ {\n\t\t\talias /data/web_static/current/;\n\t\t}\n\t}\n' /etc/nginx/nginx.conf
+service nginx restart
