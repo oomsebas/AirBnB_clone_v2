63b350385752701073e874694bcd28ef58e5f853
Ana-Morales <moralesanaruth@gmail.com>
2020-12-22 18:32:52 +0000

    Fix argument of sed command

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,3 +9,4 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i '/server_name _;/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
 service nginx restart
+exit 0
