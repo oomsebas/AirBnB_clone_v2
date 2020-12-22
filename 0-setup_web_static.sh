9af52dca66fb41effc0bbc8e426a8a23b27e0f85
Ana-Morales <moralesanaruth@gmail.com>
2020-12-22 18:40:33 +0000

    Change the order of mkdir

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,11 +2,10 @@
 # Sets up your web servers for the deployment of web_static
 sudo apt-get -y update
 sudo apt-get -y install nginx
-mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
 echo "Testing file" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i '/server_name _;/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
 service nginx restart
-exit 0
