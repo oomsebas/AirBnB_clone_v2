af5052863df8157074e66a1545531cada62b4a4e
Sebastian_Orozco <omsebas@gmail.com>
2021-01-19 20:09:14 +0000

    task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,17 +6,19 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
 def teardown_storage(exception):
     storage.close()
+
 
 @app.route('/states_list', strict_slashes=False)
 def list_states():
     states_list = storage.all(State)
     res = {}
     for states in states_list.values():
-        res[states.name] = {'id' : states.id, 'name': states.name}
-    return render_template('7-states_list.html', states_lt = res)
+        res[states.name] = {'id': states.id, 'name': states.name}
+    return render_template('7-states_list.html', states_lt=res)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
