74dce0452f03d5fc8f958eee1864859106ad1973
Sebastian_Orozco <omsebas@gmail.com>
2021-01-20 18:25:56 +0000

    Task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_world():
         """ route that return hello world """
-        return 'Hello, HBNB!'
+        return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
